6c5a9b6cc7d8ed28e4adf9a78e70eef394c3dc6f
ayako <ayomori@microsoft.com>
2018-04-18 14:38:01 +0900

    First Commit

---
+++ MultiLangBot_V4M3/MultiLangBot.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Bot;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Core.Extensions;
+using Microsoft.Bot.Schema;
+
+namespace MultiLangBot_V4M3
+{
+    public class MultiLangBot : IBot
+    {
+        public async Task OnTurn(ITurnContext context)
+        {
+            if (context.Activity.Type is ActivityTypes.Message)
+            {
+                switch (context.Activity.Text)
+                {
+                    case "en":
+                        Resources.Global.Culture = new System.Globalization.CultureInfo("en-US");
+                        break;
+                    case "ja":
+                        Resources.Global.Culture = new System.Globalization.CultureInfo("ja-JP");
+                        break;
+                    case "fr":
+                        Resources.Global.Culture = new System.Globalization.CultureInfo("fr-FR");
+                        break;
+                    case "de":
+                        Resources.Global.Culture = new System.Globalization.CultureInfo("de-DE");
+                        break;
+                    default:
+                        Resources.Global.Culture = System.Globalization.CultureInfo.CurrentCulture;
+                        break;
+                }
+
+                await context.SendActivity(Resources.Global.WELCOME_MSG);
+                await context.SendActivity(Resources.Global.CHANGE_LANG_MSG);
+
+            }
+        }
+
+    }
+}
---
+++ MultiLangBot_V4M3/Startup.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Bot.Builder.BotFramework;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
+
+namespace MultiLangBot_V4M3
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(_ => Configuration);
+
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                var supportedCultures = new[]
+                {
+                new CultureInfo("en"),
+                new CultureInfo("ja"),
+                new CultureInfo("fr"),
+                new CultureInfo("de")
+                };
+
+                options.DefaultRequestCulture = new RequestCulture(culture: "en-us", uiCulture: "en-us");
+                //options.SupportedCultures = supportedCultures;
+                //options.SupportedUICultures = supportedCultures;
+            });
+
+            services.AddBot<MultiLangBot>(options =>
+            {
+                options.CredentialProvider = new ConfigurationCredentialProvider(Configuration);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseBotFramework();
+
+            var localizationOption = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
+            app.UseRequestLocalization(localizationOption.Value);
+
+        }
+    }
+}
